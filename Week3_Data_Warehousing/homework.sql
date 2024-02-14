02196f9286698d43a7b94455bcc2c9001fd2a0e6
Ameenah21 <aminatlawal21@gmail.com>
2024-02-14 12:10:04 +0100

    edit homework.sql

--- Week3_Data_Warehousing/homework.sql
+++ Week3_Data_Warehousing/homework.sql
@@ -15,32 +15,34 @@ SELECT count(*) FROM `Green_taxi_2022.green_taxi_tables_2022`
 
 -- Write a query to count the distinct number of PULocationIDs for the entire dataset on both the tables.
 --What is the estimated amount of data that will be read when this query is executed on the External Table and the Table?
-0MB, 6.41MB
-select count(distinct PULocationID) from `seventh-sensor-412710.Green_taxi_2022.green_taxi_tables_2022`
-select count(distinct PULocationID) from `seventh-sensor-412710.Green_taxi_2022.green_taxi_tables_2022_non_partitoned`
+---- Query scans 0 MB
+select count(distinct PULocationID) from `Green_taxi_2022.green_taxi_tables_2022`
+---- Query scans 6.41 MB
+select count(distinct PULocationID) from `Green_taxi_2022.green_taxi_tables_2022_non_partitoned`
 
 --Write a query to retrieve the distinct PULocationID between lpep_pickup_datetime 06/01/2022 and 06/30/2022 (inclusive)
 
-Use the materialized table you created earlier in your from clause and note the estimated bytes. Now change the table in the from clause to the partitioned table you created for question 4 and note the estimated bytes processed. What are these values?
+--Use the materialized table you created earlier in your from clause and note the estimated bytes. Now change the table in the from clause to the partitioned table you created for question 4 and note the estimated bytes processed. What are these values?
 
 select count(distinct PULocationID) from `seventh-sensor-412710.Green_taxi_2022.green_taxi_tables_2022_non_partitoned`
 select count(distinct PULocationID) from `seventh-sensor-412710.Green_taxi_2022.green_taxi_tables_2022`
---Creating a clustered table
--- Creating a partition and cluster table
+
+-- Creating a partitioned and clustered table
 CREATE OR REPLACE TABLE `Green_taxi_2022.partitioned_green_taxi`
 PARTITION BY DATE(lpep_pickup_datetime)
 CLUSTER BY PULocationID AS
 SELECT * FROM Green_taxi_2022.green_taxi_tables_2022;
 
 --Write a query to retrieve the distinct PULocationID between lpep_pickup_datetime 06/01/2022 and 06/30/2022 (inclusive)
-
 --Use the materialized table you created earlier in your from clause and note the estimated bytes. Now change the table in the from clause to the partitioned table you created for question 4 and note the estimated bytes processed. What are these values?
 
 12.82Mb and 1.16mb
+-- Query scans 12.82 MB
 SELECT count(distinct PULocationID) as trips
 FROM Green_taxi_2022.green_taxi_tables_2022_non_partitoned
 WHERE DATE(lpep_pickup_datetime) BETWEEN '2022-06-01' AND '2022-07-01';
 
+---- Query scans 1.16 MB
 SELECT count(distinct PULocationID) as trips
 FROM Green_taxi_2022.partitioned_green_taxi
 WHERE DATE(lpep_pickup_datetime) BETWEEN '2022-06-01' AND '2022-07-01';
