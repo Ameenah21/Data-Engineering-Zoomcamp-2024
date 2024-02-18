6a38befbac45138b08517fbca71cb35d069bed58
Ameenah21 <aminatlawal21@gmail.com>
2024-02-18 21:37:12 +0100

    added the files for dlt workshop and homework

--- Week3_Data_Warehousing/homework.sql
+++ Week3_Data_Warehousing/homework.sql
@@ -20,8 +20,11 @@ select count(distinct PULocationID) from `Green_taxi_2022.green_taxi_tables_2022
 ---- Query scans 6.41 MB
 select count(distinct PULocationID) from `Green_taxi_2022.green_taxi_tables_2022_non_partitoned`
 
---Write a query to retrieve the distinct PULocationID between lpep_pickup_datetime 06/01/2022 and 06/30/2022 (inclusive)
+--Question 3. How many records have a fare_amount of 0?
+select count(*) from `Green_taxi_2022.green_taxi_tables_2022`
+where fare_amount =
 
+--Write a query to retrieve the distinct PULocationID between lpep_pickup_datetime 06/01/2022 and 06/30/2022 (inclusive)
 --Use the materialized table you created earlier in your from clause and note the estimated bytes. Now change the table in the from clause to the partitioned table you created for question 4 and note the estimated bytes processed. What are these values?
 
 select count(distinct PULocationID) from `seventh-sensor-412710.Green_taxi_2022.green_taxi_tables_2022_non_partitoned`
